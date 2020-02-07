271958b169c847486b10c1cb1fc63ba28cbff01c
eugene teu <eugeneteu@gmail.com>
2020-02-08 01:14:23 +0800

    added express hello world

--- backend/app.js
+++ backend/app.js
@@ -38,4 +38,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// testing 
+const port = 3000;
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'))
+
 module.exports = app;
