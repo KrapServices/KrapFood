f16df0ee551b4d9914bb3644d1cf214d208825e5
eugene teu <eugeneteu@gmail.com>
2020-02-19 01:10:10 +0800

    added it to load from .env

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+  console.log(process.env);
+}
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testRouter = require('./routes/testAPI');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var testRouter = require("./routes/testAPI");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/', testRouter);
+app.use("/users", usersRouter);
+app.use("/", testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,19 +37,16 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-// testing 
-const port = 3001;
+var port = process.env.port || 3000;
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 module.exports = app;
