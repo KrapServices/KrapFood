b9376b23d38eb49c5e7e68602e33d3e990e99e19
uberSaiyan <yanwl.note@gmail.com>
2020-02-26 22:02:35 +0800

    Update restaurant tables to include timestamps

--- database/create_restaurants.sql
+++ database/create_restaurants.sql
@@ -3,13 +3,17 @@ CREATE TABLE restaurants (
   threshold NUMERIC(10, 2),
   restaurant_name TEXT,
   restaurant_location TEXT,
-  delivery_fee NUMERIC(10, 2)
+  delivery_fee NUMERIC(10, 2),
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  modified_at TIMESTAMP DEFAULT current_timestamp
 );
 
 CREATE TABLE menus (
   menu_id SERIAL PRIMARY KEY,
   restaurant_id INTEGER UNIQUE,
-  FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id)
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id),
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  modified_at TIMESTAMP DEFAULT current_timestamp
 );
 
 CREATE TABLE foods (
@@ -18,5 +22,7 @@ CREATE TABLE foods (
   food_name TEXT,
   price NUMERIC(10, 2),
   menu_id INTEGER NOT NULL,
-  FOREIGN KEY (menu_id) REFERENCES menus (menu_id) 
+  FOREIGN KEY (menu_id) REFERENCES menus (menu_id),
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  modified_at TIMESTAMP DEFAULT current_timestamp
 );
