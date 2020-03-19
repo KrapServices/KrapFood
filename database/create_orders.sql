2e1e10b2f3e10a853b9ae24497b9148ad436bbca
uberSaiyan <yanwl.note@gmail.com>
2020-03-19 18:38:13 +0800

    Fix orders schema

--- database/create_orders.sql
+++ database/create_orders.sql
@@ -1,35 +1,32 @@
 CREATE TABLE orders 
 (
     order_id SERIAL PRIMARY KEY,
-    total_cost NUMERIC(10,2),
-    status text , 
+    total_cost NUMERIC(10, 2) NOT NULL,
+    CONSTRAINT positive_total_cost CHECK (total_cost > 0),
+    status TEXT NOT NULL DEFAULT 'preparing', -- 'preparing' | 'delivering' | 'completed' 
+    CHECK (status IN ('preparing', 'delivering', 'completed')),
+    delivery_location TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT current_timestamp,
     modified_at TIMESTAMP DEFAULT current_timestamp
 );
-/*Order can have multiple promotions*/
+
+-- Order can have multiple promotions
 CREATE TABLE promotions
 (
     promo_id SERIAL PRIMARY KEY,
-    order_id SERIAL,
-    discount DECIMAL(5,2), 
-    start_time DATE,
-    end_time DATE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
-);
-
-/*Order can only have one location*/
-CREATE TABLE locations 
-(
-    order_id SERIAL PRIMARY KEY,
-    address text,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+    order_id SERIAL NOT NULL,
+    discount INTEGER NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 
 CREATE TABLE contain
 (
     order_id SERIAL, 
     food_id SERIAL,
-    quantity int,
+    quantity INTEGER NOT NULL,
+    CONSTRAINT positive_quantity CHECK (quantity > 0),
     PRIMARY KEY (order_id, food_id),
     FOREIGN KEY (food_id) REFERENCES foods(food_id),
     FOREIGN KEY (order_id) REFERENCES orders(order_id)
