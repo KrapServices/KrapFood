38de263cc9c478cfcfb15743869fc33e326e1505
uberSaiyan <yanwl.note@gmail.com>
2020-03-22 13:33:20 +0800

    Improve manager dashboard

--- frontend/src/components/views/manager_view/Manager.jsx
+++ frontend/src/components/views/manager_view/Manager.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {
+  Form, Table, Button, Divider, Message, Header,
+} from 'semantic-ui-react';
 import userContext from '../../../userContext';
 import config from '../../../config.json';
 
@@ -7,17 +10,12 @@ class Manager extends Component {
   constructor() {
     super();
     this.state = {
-      password: '',
-      riders: [
-        {
-          id: 1,
-          status: 'part-time',
-        },
-        {
-          id: 2,
-          status: 'full-time',
-        },
-      ],
+      newPassword: '',
+      confirmPassword: '',
+      loading: false,
+      success: false,
+      error: false,
+      riders: [],
     };
   }
 
@@ -29,71 +27,132 @@ class Manager extends Component {
     });
   }
 
-  submitPassword() {
-    const { user } = this.context;
-    const { password } = this.state;
-    axios.patch(`${config.localhost}managers/`, {
-      manager_id: user.manager_id,
-      password,
+  handleChange(event, { name, value }) {
+    this.setState({
+      [name]: value,
     });
   }
 
+  async handleSubmit() {
+    const { user } = this.context;
+    const { newPassword } = this.state;
+    this.setState({
+      newPassword: '',
+      confirmPassword: '',
+      loading: true,
+    });
+    try {
+      await axios.patch(`${config.localhost}managers/`, {
+        manager_id: user.manager_id,
+        password: newPassword,
+      });
+      this.setState({
+        loading: false,
+        success: true,
+        error: false,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        success: false,
+        error: true,
+      });
+    }
+  }
+
   render() {
-    const { password, riders } = this.state;
+    const {
+      newPassword, confirmPassword, loading, success, error, riders,
+    } = this.state;
 
     return (
       <>
-        <h3>Update password</h3>
-        <div className="ui action input">
-          <input
-            type="password"
-            placeholder="Enter new password"
-            value={password}
-            onChange={(event) => {
-              this.setState({
-                password: event.target.value,
-              });
-            }}
-          />
-          <button
-            className="ui button"
-            type="button"
-            onClick={() => {
-              this.submitPassword();
-              this.setState({
-                password: '',
-              });
-            }}
+        <div
+          style={{
+            paddingLeft: '35vw',
+            paddingRight: '35vw',
+          }}
+        >
+          <Header size="huge">Update password</Header>
+          <Form
+            loading={loading}
+            success={success}
+            error={error}
+            onSubmit={() => this.handleSubmit()}
           >
-            Submit
-          </button>
+            <Form.Field>
+              <Form.Input
+                name="newPassword"
+                value={newPassword}
+                label="Enter New Password"
+                type="password"
+                placeholder="Enter password"
+                required
+                onChange={(event, data) => this.handleChange(event, data)}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Form.Input
+                name="confirmPassword"
+                value={confirmPassword}
+                label="Confirm New Password"
+                type="password"
+                placeholder="Enter your password again"
+                required
+                onChange={(event, data) => this.handleChange(event, data)}
+                error={newPassword !== confirmPassword}
+              />
+            </Form.Field>
+            <Form.Field />
+            <Message
+              success
+              header="Update successful"
+              content="Your password has been updated."
+            />
+            <Message
+              error
+              header="An error occurred"
+              content="Failed to update your password."
+            />
+            <Button
+              type="submit"
+              disabled={newPassword.length === 0 || newPassword !== confirmPassword}
+            >
+              Submit
+            </Button>
+          </Form>
         </div>
 
+        <Divider />
+
         <div style={{
-          marginTop: '32px',
+          paddingLeft: '10vw',
+          paddingRight: '10vw',
         }}
         >
-          <h3>My riders</h3>
-          <table className="ui celled table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Part-time/Full-time</th>
-              </tr>
-            </thead>
-            <tbody>
+          <Header size="huge">My riders</Header>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Rider ID</Table.HeaderCell>
+                <Table.HeaderCell>Part-time/Full-time</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
               {riders.map((rider) => {
                 const { id, status } = rider;
-
                 return (
-                  <tr key={id}>
-                    <td>{id}</td>
-                    <td>{status === 'part-time' ? 'Part-time' : 'Full-time'}</td>
-                  </tr>
+                  <Table.Row key={id}>
+                    <Table.Cell>
+                      {id}
+                    </Table.Cell>
+                    <Table.Cell>{status === 'part-time' ? 'Part-time' : 'Full-time'}</Table.Cell>
+                  </Table.Row>
                 );
               })}
-            </tbody>
-          </table>
+            </Table.Body>
+          </Table>
         </div>
       </>
     );
