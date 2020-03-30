1cc70c15f08f1efb0f837d5e2762cb71522c5f06
eugene teu <eugeneteu@gmail.com>
2020-03-30 22:55:33 +0800

    fix modal update

--- frontend/src/components/views/staff_view/MenuItems.jsx
+++ frontend/src/components/views/staff_view/MenuItems.jsx
@@ -5,67 +5,67 @@ import config from '../../../config.json';
 import userContext from '../../../userContext';
 
 class MenuItems extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          menu: [],
-          restaurant_id: '',
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: [],
+      restaurant_id: '',
+    };
 
-      this.loadMenu =  async () => {
-        const restaurant_id = this.context.user.restaurant_id;
-        this.state.restaurant_id = restaurant_id;
-        try {
-            const result = await Axios.get(
-            `${config.localhost}restaurants/${restaurant_id}`);
-            console.log(result);
-            if (result.status === 200) {
-                this.setState({ menu: result.data.restaurant.food});
-            } else {
-                alert('unable to load menu');
-            }
-        } catch (error) {
-            console.log("Error has occured");
-        }
-      };
-    }
-
-    /* executed once components are mounted */
-    componentDidMount() {
-        this.loadMenu();
-      }
-
-    render() {
-        const menu = this.state.menu;
-        console.log(menu);
-        return (
-            <Item.Group divided style={{ textAlign: 'left' }}>
-            {menu.map((food) => (
-              <Item key={food.food_id}>
-                <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                <Item.Content>
-                  <Item.Header as="h1">{food.food_name}</Item.Header>
-                  <Item.Description floated="left">
-                    Price:
-                    <b>{` ${food.price}`}</b>
-                    {' '}
-                    <br />
-                    Daily Limit:
-                    {' '}
-                    <b>{` ${food.daily_limit}`}</b>
-                    {' '}
-                    <br />
-                    Available:
-                    <b>{food.availability ? <Icon name="checkmark" color="green" /> : <Icon name="cancel" color="red" />}</b>
-                    {' '}
-                  </Item.Description>
-                </Item.Content>
-              </Item>
-            ))}
-          </Item.Group>
+    this.loadMenu = async () => {
+      const { restaurant_id } = this.context.user;
+      this.state.restaurant_id = restaurant_id;
+      try {
+        const result = await Axios.get(
+          `${config.localhost}restaurants/${restaurant_id}`,
         );
-    }
+        console.log(result);
+        if (result.status === 200) {
+          this.setState({ menu: result.data.restaurant.food });
+        } else {
+          alert('unable to load menu');
+        }
+      } catch (error) {
+        console.log('Error has occured');
+      }
+    };
+  }
 
+  /* executed once components are mounted */
+  componentDidMount() {
+    this.loadMenu();
+  }
+
+  render() {
+    const { menu } = this.state;
+    console.log(menu);
+    return (
+      <Item.Group divided style={{ textAlign: 'left' }}>
+        {menu.map((food) => (
+          <Item key={food.food_id}>
+            <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+            <Item.Content>
+              <Item.Header as="h1">{food.food_name}</Item.Header>
+              <Item.Description floated="left">
+                Price:
+                <b>{` ${food.price}`}</b>
+                {' '}
+                <br />
+                Daily Limit:
+                {' '}
+                <b>{` ${food.daily_limit}`}</b>
+                {' '}
+                <br />
+                Available:
+                <b>{food.availability ? <Icon name="checkmark" color="green" /> : <Icon name="cancel" color="red" />}</b>
+                {' '}
+              </Item.Description>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    );
+  }
 }
 
 MenuItems.contextType = userContext;
--- frontend/src/components/views/staff_view/StaffSummary.jsx
+++ frontend/src/components/views/staff_view/StaffSummary.jsx
@@ -1,46 +1,44 @@
 import React, { Component } from 'react';
-import userContext from '../../../userContext';
 import Axios from 'axios';
-import config from '../../../config.json';
 import { Table, Statistic } from 'semantic-ui-react';
+import userContext from '../../../userContext';
+import config from '../../../config.json';
 
 class StaffSummary extends Component {
-    constructor() {
-        super();
-        this.state = {};
+  constructor() {
+    super();
+    this.state = {};
 
-      this.loadOrders =  async () => {
-        const restaurant_id = this.context.user.restaurant_id;
-        this.state.restaurant_id = restaurant_id;
-        try {
-            const result = await Axios.get(
-                `${config.localhost}restaurants/stats/orders${restaurant_id}`
-            )
-            console.log(result);
-            if (result.status === 200) {
-                this.setState({ menu: result.data});
-            } else {
-                alert('unable to load menu');
-            }
-        return result.data.totalOrders;
-        } catch (error) {
-            console.log("Error has occured");
+    this.loadOrders = async () => {
+      const { restaurant_id } = this.context.user;
+      this.state.restaurant_id = restaurant_id;
+      try {
+        const result = await Axios.get(
+          `${config.localhost}restaurants/stats/orders${restaurant_id}`,
+        );
+        console.log(result);
+        if (result.status === 200) {
+          this.setState({ menu: result.data });
+        } else {
+          alert('unable to load menu');
         }
-      };
-    
+        return result.data.totalOrders;
+      } catch (error) {
+        console.log('Error has occured');
+      }
     };
+  }
 
-    render() {
-        return (
-            <Table>
-            <Statistic>
-            <Statistic.Value>400</Statistic.Value>
-            <Statistic.Label>Orders</Statistic.Label>
-            </Statistic>
-            </Table>
-        ); 
-    }
-
+  render() {
+    return (
+      <Table>
+        <Statistic>
+          <Statistic.Value>400</Statistic.Value>
+          <Statistic.Label>Orders</Statistic.Label>
+        </Statistic>
+      </Table>
+    );
+  }
 }
 
 StaffSummary.contextType = userContext;
