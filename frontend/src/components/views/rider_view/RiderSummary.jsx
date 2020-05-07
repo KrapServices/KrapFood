fa5c02ac75e669dace5eecc8e74bfe7334b95401
hello <linmeian@gmail.com>
2020-05-07 12:52:05 +0800

    remove debug

--- frontend/src/components/views/rider_view/RiderSummary.jsx
+++ frontend/src/components/views/rider_view/RiderSummary.jsx
@@ -27,8 +27,6 @@ class RiderSummary extends React.Component {
     const url = `${config.localhost}riders/${rider_id}/ridersummary/?startDate=${primitiveStartDate}&endDate=${primitiveEndDate}`;
     const response = (await axios.get(url));
     const summary = response.data;
-    console.log(summary[0]);
-    console.log('test');
     this.setState({
       riderStats: summary[0],
     });
